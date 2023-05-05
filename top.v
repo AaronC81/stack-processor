5c23ceb3321fd991f9922126d8ded7e52db343fd
Aaron Christiansen <aaronc20000@gmail.com>
2023-05-05 23:46:42 +0100

    7seg driver

--- top.v
+++ top.v
@@ -1,9 +1,66 @@
+module seven_seg (
+    input clk, input [4:0] digit,
+    output A, output B, output C, output D, output E, output F, output G
+);
+    //   a
+    // f   b
+    //   g
+    // e   c
+    //   d
+
+    reg [6:0] state;
+    assign A = state[6];
+    assign B = state[5];
+    assign C = state[4];
+    assign D = state[3];
+    assign E = state[2];
+    assign F = state[1];
+    assign G = state[0];
+
+    always @(posedge clk) begin
+        case (digit)
+            5'd0: state <= 7'b1111110;
+            5'd1: state <= 7'b0110000;
+            5'd2: state <= 7'b1101101;
+            5'd3: state <= 7'b1111001;
+            5'd4: state <= 7'b0110011;
+            5'd5: state <= 7'b1011011;
+            5'd6: state <= 7'b1011111;
+            5'd7: state <= 7'b1110000;
+            5'd8: state <= 7'b1111111;
+            5'd9: state <= 7'b1111011;
+
+            default: state <= 7'b0000001;
+        endcase
+    end
+endmodule
+
 module top (
     input CLK,
     output LED,
-    output USBPU
+    output USBPU,
+
+    output PIN_15, // A
+    output PIN_14, // B
+    output PIN_13, // C
+    output PIN_12, // D
+    output PIN_11, // E
+    output PIN_16, // F
+    output PIN_17, // G
 );
     assign USBPU = 0;
+
+    seven_seg seven_seg_inst(
+        .clk(CLK),
+        .digit(instruction_index),
+        .A(PIN_15),
+        .B(PIN_14),
+        .C(PIN_13),
+        .D(PIN_12),
+        .E(PIN_11),
+        .F(PIN_16),
+        .G(PIN_17),
+    );
 
     wire [7:0] instructions [0:3];
     assign instructions[0] = 8'b00000000; // nop
