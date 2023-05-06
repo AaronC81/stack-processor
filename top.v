3a9c1e95ac9023b2913ba9669a0e4c0f78beb8e4
Aaron Christiansen <aaronc20000@gmail.com>
2023-05-06 10:53:52 +0100

    Add a stack

--- top.v
+++ top.v
@@ -1,5 +1,5 @@
 module seven_seg (
-    input clk, input [4:0] digit,
+    input clk, input [4:0] digit, input valid,
     output A, output B, output C, output D, output E, output F, output G
 );
     //   a
@@ -18,20 +18,23 @@ module seven_seg (
     assign G = state[0];
 
     always @(posedge clk) begin
-        case (digit)
-            5'd0: state <= 7'b1111110;
-            5'd1: state <= 7'b0110000;
-            5'd2: state <= 7'b1101101;
-            5'd3: state <= 7'b1111001;
-            5'd4: state <= 7'b0110011;
-            5'd5: state <= 7'b1011011;
-            5'd6: state <= 7'b1011111;
-            5'd7: state <= 7'b1110000;
-            5'd8: state <= 7'b1111111;
-            5'd9: state <= 7'b1111011;
+        if (valid) begin
+            case (digit)
+                5'd0: state <= 7'b1111110;
+                5'd1: state <= 7'b0110000;
+                5'd2: state <= 7'b1101101;
+                5'd3: state <= 7'b1111001;
+                5'd4: state <= 7'b0110011;
+                5'd5: state <= 7'b1011011;
+                5'd6: state <= 7'b1011111;
+                5'd7: state <= 7'b1110000;
+                5'd8: state <= 7'b1111111;
+                5'd9: state <= 7'b1111011;
 
-            default: state <= 7'b0000001;
-        endcase
+                default: state <= 7'b0000001;
+            endcase
+        end
+        else state <= 7'b0000001;
     end
 endmodule
 
@@ -46,27 +49,45 @@ module top (
     output PIN_12, // D
     output PIN_11, // E
     output PIN_16, // F
-    output PIN_17, // G
+    output PIN_17  // G
 );
     assign USBPU = 0;
 
     seven_seg seven_seg_inst(
         .clk(CLK),
-        .digit(instruction_index),
+        .digit(stack_top_item),
+        .valid(stack_not_empty),
         .A(PIN_15),
         .B(PIN_14),
         .C(PIN_13),
         .D(PIN_12),
         .E(PIN_11),
         .F(PIN_16),
-        .G(PIN_17),
+        .G(PIN_17)
     );
 
+    reg [31:0] stack [0:255];
+    reg [7:0] stack_pointer = 255;
+
+    wire [31:0] stack_top_item;
+    assign stack_top_item = stack[stack_pointer + 1];
+
+    wire stack_is_empty;
+    assign stack_is_empty = stack_pointer == 255;
+    wire stack_not_empty;
+    assign stack_not_empty = ~stack_is_empty;
+
+    integer i;
+    initial begin
+        for (i = 0; i <= 255; i = i + 1)
+            stack[i] = 0;
+    end
+
     wire [7:0] instructions [0:3];
-    assign instructions[0] = 8'b00000000; // nop
-    assign instructions[1] = 8'b00000010; // off
-    assign instructions[2] = 8'b00000001; // nop
-    assign instructions[3] = 8'b00000011; // on
+    assign instructions[0] = 8'b00000000;
+    assign instructions[1] = 8'b00000010;
+    assign instructions[2] = 8'b00000010;
+    assign instructions[3] = 8'b00000011;
 
     reg [1:0] instruction_index = 0;
 
@@ -79,15 +100,40 @@ module top (
 
     reg led = 0;
     assign LED = led;
+
+    // We can't read and write the stack in the same cycle, so do this instead
+    reg stack_write_back;
+    reg [31:0] stack_write_back_value = 0;
+
+    // TODO: briefly flickers incorrect pushed value on display, because SP has incremented but new value hasn't been written back
     always @(posedge instruction_clock) begin
-        case (instructions[instruction_index])
-            8'b00000000:;
-            8'b00000010: led <= 0;
-            8'b00000011: led <= 1;
-            default:;
-        endcase
+        led <= ~led;
+
+        if (stack_write_back) begin
+            stack_write_back <= 0;
+            stack[stack_pointer + 1] <= stack_write_back_value;
+        end
+        else begin
+            case (instructions[instruction_index])
+                8'b00000000: begin
+                    stack_pointer = stack_pointer + 1;
+
+                    stack_write_back <= 1;
+                    stack_write_back_value <= 0;
+                end
+                8'b00000010: begin
+                    stack_write_back <= 1;
+                    stack_write_back_value <= stack_top_item + 1;
+                end
+                8'b00000011: stack_pointer <= stack_pointer - 1;
+                default:;
+            endcase
+        end
     end
+
     always @(negedge instruction_clock) begin
-        instruction_index <= instruction_index + 1;
+        if (~stack_write_back) begin
+            instruction_index <= instruction_index + 1;
+        end
     end
 endmodule
